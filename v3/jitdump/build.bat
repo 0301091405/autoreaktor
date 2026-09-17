6745587e739c1aab8410d0e135bdc624bf76ac1e
0301091405 <sihirbaz214@gmail.com>
2026-09-17 04:23:29 +0300

    cleanup: test binaries out of the tree

--- v3/jitdump/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" >nul 2>&1
-cl /nologo /LD /EHsc /O2 /std:c++17 clrjit_hook.cpp /Fe:clrjit_dump.dll
-echo BUILD_RC=%ERRORLEVEL%
